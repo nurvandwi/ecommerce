e1e0bafe40a92eb3a6b5c880dbe6ea956f593c71
Nurvandwi <nurvandwi20@gmail.com>
2020-07-14 16:00:20 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import WomanShayna from "@/components/WomanShayna.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnerShayna,
+    Footer
+  }
+};
+</script>
